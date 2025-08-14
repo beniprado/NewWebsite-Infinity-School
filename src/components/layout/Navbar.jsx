b17c718a2fb2f49a3c81ffcba66bbbb44ec1ccbb
beniprado <benignosprado@gmail.com>
2025-08-14 12:40:53 -0300

    bug fix logo

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import BtnInfo from "../ui/BtnInfo"
 import ListDropdown from "../ui/ListDropdown"
-import Logo from "../../assets/infinitylogo.png"
+import logo from "../../assets/logo.png"
 
 const NavList = "text-lg font-medium cursor-pointer"
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between p-5 px-15">
-      <img src={Logo} alt="logo" />
+      <img src={logo} alt="logo" />
       <ul className="hidden xl:flex items-center gap-10">
         <li className={NavList}>Home</li>
         <li className={NavList}>Sobre</li>
