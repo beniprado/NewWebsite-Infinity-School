8b18dbea2e3d56901b0fc3896e1ee280c16d1573
beniprado <benignosprado@gmail.com>
2025-08-14 23:04:29 -0300

    bug fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import Resultados from "./section/Resultados";
 import Avaliacoes from "./section/avaliacoes";
 import Cta from "../../components/ui/Cta";
 import Visita from "./section/Visita";
-import Empresas from "./section/empresas";
+import Empresas from "./section/Empresas";
 
 const Home = () => {
     return <main className=" bg-black">
